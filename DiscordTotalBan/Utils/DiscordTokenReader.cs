e7341800ccbe3fee016a2abf94cfd93a5d616839
oqo0 <oq.roman@gmail.com>
2024-05-23 16:46:54 +0300

    Implement environment variable token reader

---
+++ DiscordTotalBan/Utils/DiscordTokenReader.cs
@@ -0,0 +1,18 @@
+﻿namespace DiscordTotalBan.Utils;
+
+public static class DiscordTokenReader
+{
+    private const string DiscordTokenEnvironmentVariableName = "DISCORD_TOKEN";
+    
+    public static string GetToken()
+    {
+        var token = Environment.GetEnvironmentVariable(DiscordTokenEnvironmentVariableName);
+        if (token is null)
+        {
+            throw new ArgumentNullException(
+                nameof(token), "Environment variable DISCORD_TOKEN was not found");
+        }
+
+        return token;
+    }
+}
